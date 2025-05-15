51edd94204f0a865f074d4b5e0ca5ab22dfa0bb1
Paul ZOBLATIN <zpaulange@gmail.com>
2025-05-16 00:37:51 +0100

    dataTable end first

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,77 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { useState } from 'react'
 import './App.css'
-import Select from './ui/Select/Select'
 import DataTable from './ui/DataTable/DataTable'
 
 function App() {
+  const [selectedFilters, setSelectedFilters] = useState<{ [key: string]: any }>({});
+  console.log('selectedFilters', selectedFilters);
+
+  const data = [
+    {
+      id: 1,
+      name: 'John Doe',
+      status: 2,
+      age: 25,
+      country: 'Benin',
+    },
+    {
+      id: 2,
+      name: 'Jane Doe',
+      status: 1,
+      age: 30,
+      country: 'France',
+    },
+    {
+      id: 3,
+      name: 'Bob Smith',
+      status: 2,
+      age: 35,
+      country: 'United States',
+    },
+    {
+      id: 4,
+      name: 'Alice Johnson',
+      status: 1,
+      age: 40,
+      country: 'Canada',
+    },
+    {
+      id: 5,
+      name: 'Charlie Brown',
+      status: 2,
+      age: 45,
+      country: 'United Kingdom',
+    },
+  ]
+
+  const thead = ["ID", "Name", "Status", "Age", "Country"]
+
+  const renderRow = (item:any) => {
+    return (
+      <tr key={item.id}>
+        <td>{item.id}</td>
+        <td>{item.name}</td>
+        <td>{item.status}</td>
+        <td>{item.age}</td>
+        <td>{item.country}</td>
+      </tr>
+    );
+  }
+  // Les clés pour lesquelles tu veux générer un filtre
+  const filterKeys = ['status', 'country'];
   
   return (
     <>
       <div className='box'>
         <DataTable
           filters={{
-            defaultFilter:true,
             data:[
               {
+                label:{
+                  title:"Status",
+                  name:"status"
+                },
                 options:[
                   { label: 'tout', value: 'Tout', count: 5 },
                   { label: 'Option 1', value: 'option1', statusColor: 'red', count: 5 },
@@ -22,6 +80,10 @@ function App() {
                 ]
               },
               {
+                label:{
+                  title:"Nom",
+                  name:"nom"
+                },
                 options:[
                   { label: 'tout', value: 'Tout', count: 5 },
                   { label: 'Option 1', value: 'option1', statusColor: 'red', count: 5 },
@@ -31,12 +93,14 @@ function App() {
               }
             ]
           }}
-          data={[]}
+          data={data}
+          thead={thead}
+          renderRow={renderRow}
+          selectedFilters={selectedFilters}
+          setSelectedFilters={setSelectedFilters}
         />
       </div>
-      <div className='box'>
-        <DataTable/>
-      </div>
+      <pre>{JSON.stringify(selectedFilters, null, 2)}</pre>
     </>
   )
 }
--- src/ui/DataTable/DataTable.tsx
+++ src/ui/DataTable/DataTable.tsx
@@ -1,23 +1,40 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import { useState } from "react";
 import Select from "../Select/Select";
 import styles from "./dataTable.module.scss";
 
 interface Filter {
-  defaultFilter?: boolean
-  data?: Array<any>
+  //defaultFilter?: boolean
+  data?: Array<any>;
 }
 
 interface DataTableProps {
   filters?: Filter;
-  data: Array<any>
+  data: Array<any>;
+  thead?: Array<any>;
+  renderRow?: (item: any, idx: number) => React.ReactNode;
+  selectedFilters: { [key: string]: any };
+  setSelectedFilters: React.Dispatch<
+    React.SetStateAction<{ [key: string]: any }>
+  >;
 }
 export default function DataTable(props: DataTableProps) {
-  const { filters, data } = props;
-  console.log(filters);
+  const {
+    filters,
+    data,
+    thead,
+    renderRow,
+    selectedFilters,
+    setSelectedFilters,
+  } = props;
+  console.log("filters", filters);
+  // État pour stocker toutes les valeurs sélectionnées par name
+  //const [selectedFilters, setSelectedFilters] = useState<{ [key: string]: any }>({});
+  //console.log('selectedFilters', selectedFilters);
 
   //default filter
-  const defaultFilter = filters?.defaultFilter && (
+  /* const defaultFilter = filters?.defaultFilter && (
     <Select
       options={[
         { label: "tout", value: "Tout" },
@@ -25,36 +42,77 @@ export default function DataTable(props: DataTableProps) {
         { label: "Option 2", value: "def option2", statusColor: "green", count: 10},
         { label: "Option 3", value: "def option3", statusColor: "yellow", count: 18 },
       ]}
-      onSelect={(option: any) => {}}
+      onSelect={(option: any) =>
+        setSelectedFilters(prev => ({
+          ...prev,
+          fan: option
+        }))
+      }
       statusColors={true}
       count={true}
+      label={{title:"Filtre", name:"fan"}}
     />
-  );
-
+  ); */
 
   //filter data
   interface FilterDataItem {
-    options: Array<any>
-    [key: string]: any
+    options: Array<any>;
+    label?: { title?: string; name?: string };
+    [key: string]: any;
   }
-  const filterData = filters?.data && (
+  const filterData =
+    filters?.data &&
     filters.data.map((f: FilterDataItem, idx: number) => (
-        <Select 
-            key={idx}
-            options={f.options}
-            onSelect={(option: any) => {console.log("option", option)}}
-            statusColors={!!f.statusColors}
-            count={!!f.count}
-        />
-    ))
-  )
+      <Select
+        key={idx}
+        options={f.options}
+        onSelect={(option: any) => {
+          if (f.label?.name) {
+            setSelectedFilters((prev) => ({
+              ...prev,
+              [f.label?.name ?? ""]: option,
+            }));
+          }
+        }}
+        statusColors={!!f.statusColors}
+        count={!!f.count}
+        label={f.label}
+      />
+    ));
 
   return (
     <div className={styles.datatable}>
       <div className={styles.datatable_head}>
-        {defaultFilter}
+        {/* {defaultFilter} */}
         {filterData}
       </div>
+      <table>
+        {thead && thead.length > 0 && (
+          <thead>
+            <tr>
+              {thead.map((th: any, idx: number) => (
+                <th key={idx}>{th}</th>
+              ))}
+            </tr>
+          </thead>
+        )}
+        <tbody>
+          {data && data.length > 0 ? (data.map((item: any, idx: number) =>
+            renderRow ? (renderRow(item, idx)) :(
+                <tr key={idx}>
+                  {Object.keys(item).map((key: any, idx: number) => (
+                    <td key={idx}>{item[key]}</td>
+                  ))}
+                </tr>
+              )
+            )) : (
+            <tr>
+              <td colSpan={thead?.length}>Aucun resultat</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+      <div></div>
     </div>
   );
 }
