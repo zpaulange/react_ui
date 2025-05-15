8b0d86667d5560851eb484d1c95e322ae2e91cd5
Paul ZOBLATIN <zpaulange@gmail.com>
2025-05-15 22:17:40 +0100

    datatable integration

--- src/App.tsx
+++ src/App.tsx
@@ -5,47 +5,34 @@ import Select from './ui/Select/Select'
 import DataTable from './ui/DataTable/DataTable'
 
 function App() {
-  const optionsList1 = [
-    { label: 'tout', value: 'Tout', count: 5 },
-    { label: 'Option 1', value: 'option1', statusColor: 'red', count: 5 },
-    { label: 'Option 2', value: 'option2', statusColor: 'green', count: 10},
-    { label: 'Option 3', value: 'option3', statusColor: 'yellow', count: 18 },
-  ]
-  const optionsList2 = [
-    { label: 'BJ', value: 'Benin' },
-    { label: 'NG', value: 'Nigeria' },
-    { label: 'GH', value: 'Ghana' },
-  ]
-  const [value1, setValue1] = useState(null)
-  console.log("value1", value1);
-
-  const [value2, setValue2] = useState(null)
-  console.log("value2", value2);
   
   return (
     <>
-      <div className='app'>
-        <Select
-          options={optionsList1}
-          onSelect={(option: any) => {
-            //console.log("option", option)
-            setValue1(option.value1)
-          }}
-          statusColors={true}
-          count={true}
-          label='Status'
-        />
-        <Select
-          options={optionsList2}
-          onSelect={(option: any) => {
-            //console.log("option", option)
-            setValue2(option.value)
-          }}
-          isLabel={true}
-        />
-      </div>
       <div className='box'>
-        <DataTable/>
+        <DataTable
+          filters={{
+            defaultFilter:true,
+            data:[
+              {
+                options:[
+                  { label: 'tout', value: 'Tout', count: 5 },
+                  { label: 'Option 1', value: 'option1', statusColor: 'red', count: 5 },
+                  { label: 'Option 2', value: 'option2', statusColor: 'green', count: 10},
+                  { label: 'Option 3', value: 'option3', statusColor: 'yellow', count: 18 },
+                ]
+              },
+              {
+                options:[
+                  { label: 'tout', value: 'Tout', count: 5 },
+                  { label: 'Option 1', value: 'option1', statusColor: 'red', count: 5 },
+                  { label: 'Option 2', value: 'option2', statusColor: 'green', count: 10},
+                  { label: 'Option 3', value: 'option3', statusColor: 'yellow', count: 18 },
+                ]
+              }
+            ]
+          }}
+          data={[]}
+        />
       </div>
     </>
   )
--- src/ui/DataTable/DataTable.tsx
+++ src/ui/DataTable/DataTable.tsx
@@ -1,30 +1,60 @@
-import Select from "../Select/Select"
-import styles from "./dataTable.module.scss"
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import Select from "../Select/Select";
+import styles from "./dataTable.module.scss";
 
-export default function DataTable() {
+interface Filter {
+  defaultFilter?: boolean
+  data?: Array<any>
+}
+
+interface DataTableProps {
+  filters?: Filter;
+  data: Array<any>
+}
+export default function DataTable(props: DataTableProps) {
+  const { filters, data } = props;
+  console.log(filters);
+
+  //default filter
+  const defaultFilter = filters?.defaultFilter && (
+    <Select
+      options={[
+        { label: "tout", value: "Tout" },
+        { label: "Option 1", value: "def option1", statusColor: "red", count: 5 },
+        { label: "Option 2", value: "def option2", statusColor: "green", count: 10},
+        { label: "Option 3", value: "def option3", statusColor: "yellow", count: 18 },
+      ]}
+      onSelect={(option: any) => {}}
+      statusColors={true}
+      count={true}
+    />
+  );
+
+
+  //filter data
+  interface FilterDataItem {
+    options: Array<any>
+    [key: string]: any
+  }
+  const filterData = filters?.data && (
+    filters.data.map((f: FilterDataItem, idx: number) => (
+        <Select 
+            key={idx}
+            options={f.options}
+            onSelect={(option: any) => {console.log("option", option)}}
+            statusColors={!!f.statusColors}
+            count={!!f.count}
+        />
+    ))
+  )
+
   return (
     <div className={styles.datatable}>
-        <div className={styles.datatable_head}>
-            <Select
-                options={[
-                    { label: 'tout', value: 'Tout' }, 
-                    { label: 'Option 1', value: 'option1' }, 
-                    { label: 'Option 2', value: 'option2' }, 
-                    { label: 'Option 3', value: 'option3' }
-                ]}
-                onSelect={(option: any) => {}}
-            />
-            <Select
-                options={[
-                    { label: 'tout', value: 'Tout' }, 
-                    { label: 'Option 1', value: 'option1' }, 
-                    { label: 'Option 2', value: 'option2' }, 
-                    { label: 'Option 3', value: 'option3' }
-                ]}
-                onSelect={(option: any) => {}}
-            />
-        </div>
-
+      <div className={styles.datatable_head}>
+        {defaultFilter}
+        {filterData}
+      </div>
     </div>
-  )
+  );
 }
